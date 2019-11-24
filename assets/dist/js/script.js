a9537568441fd6425065c9da58e763c324d96f2f
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-25 00:36:05 +0200

    Дополнения к разметке article-post: 1. article-post__header добавлена фотография. 2. Написаны стили и разметка для секции introduction. 3. Написаны стили и разметка для секции decription-post.

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -8,6 +8,30 @@ $(function() {
    });
 });
 
+$(document).ready(function () {
+   var assistants = $('.carousel-3D__list');
+
+   assistants.on("init", function (event, slick) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+   assistants.on("afterChange", function (event, slick, currentSlide) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+
+   assistants.slick({
+      infinite: true,
+      slidesToShow: 3,
+      arrows: true,
+      centerMode: true,
+      centerPadding: '0',
+      prevArrow: '<button type="button" class="button-arrow carousel-3D__arrow carousel-3D__arrow--left"><i class="icon-right-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow carousel-3D__arrow carousel-3D__arrow--right"><i class="icon-right-arrow"></i></button>',
+      dots: false
+   });
+});
+
 
 var $anchor = $( '.js-anchor' );
 
