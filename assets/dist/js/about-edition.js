05c89ffa40d2bfc000cd03e9bcad51e0dbdf9283
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-25 00:49:16 +0200

    Переместила скрипт карусели с общего файла js в файл для каждой страницы.

--- assets/dist/js/about-edition.js
+++ assets/dist/js/about-edition.js
@@ -75,4 +75,28 @@ $(document).ready(function () {
    $('.archive-edition__list .fancybox-link').fancybox({
       backFocus: false
    });
+
+
+   var assistants = $('.carousel-3D__list');
+
+   assistants.on("init", function (event, slick) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+   assistants.on("afterChange", function (event, slick, currentSlide) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+
+   assistants.slick({
+      infinite: true,
+      slidesToShow: 3,
+      arrows: true,
+      centerMode: true,
+      centerPadding: '0',
+      prevArrow: '<button type="button" class="button-arrow carousel-3D__arrow carousel-3D__arrow--left"><i class="icon-right-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow carousel-3D__arrow carousel-3D__arrow--right"><i class="icon-right-arrow"></i></button>',
+      dots: false
+   });
+
 });
