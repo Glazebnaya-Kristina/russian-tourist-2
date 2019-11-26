eca2e323e07b987d81da866fbcb13cbaf60d675c
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-26 11:43:08 +0200

    1. Разметка и стили для секции painting 2. Наброски для табов.

--- assets/dist/js/inspiration.js
+++ assets/dist/js/inspiration.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+   var postImg = document.querySelectorAll('.painting__article-img');
 
+
+   postImg.forEach(function (item) {
+      item.addEventListener('click', function () {
+         var post = document.querySelectorAll('.preview-article__article');
+
+         post.forEach(function (item2) {
+            item2.classList.toggle('preview-article__article--active');
+         });
+      })
+   });
 });
