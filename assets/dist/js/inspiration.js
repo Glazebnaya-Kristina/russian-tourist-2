76cd9d7cf4d39cc3a10afe8815456fe2cd3623b1
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-26 18:06:43 +0200

    1. Добавлен блок  carousel-content, и заменила ним в секции books. 2. Сделан полностью скрипт табов.

--- assets/dist/js/inspiration.js
+++ assets/dist/js/inspiration.js
@@ -1,14 +1,30 @@
 $(document).ready(function () {
-   var postImg = document.querySelectorAll('.painting__article-img');
 
+   var postImg = document.querySelectorAll('.painting__article-img');
 
    postImg.forEach(function (item) {
       item.addEventListener('click', function () {
-         var post = document.querySelectorAll('.preview-article__article');
-
-         post.forEach(function (item2) {
-            item2.classList.toggle('preview-article__article--active');
-         });
+         this.closest('.preview-article__article').classList.toggle('preview-article__article--active');
       })
+   });
+
+
+   $('.books__carousel-nav').slick({
+      slidesToShow: 19,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: false,
+      asNavFor: '.carousel-content__list',
+      centerMode: true,
+      variableWidth: true
+   });
+   $('.carousel-content__list').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<button type="button" class="button-arrow button-arrow--gold carousel-content__arrow carousel-content__arrow--left"><i class="icon-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow button-arrow--gold carousel-content__arrow carousel-content__arrow--right"><i class="icon-arrow"></i></button>',
+      fade: true,
+      asNavFor: '.books__carousel-nav'
    });
 });
