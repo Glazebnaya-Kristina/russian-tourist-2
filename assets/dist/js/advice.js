006daf0fad899a315fc7f7cfc5619452d18b2d5d
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-27 13:54:50 +0200

    1. Добавленно к кнопке в моб меню data атрибут на открытие модалки. 2. Написаны настройки для карусели equipment (Снаряжение). 3. Сделана разметка и стили для секции equipment. 4. Для блока preview-article добавлены стили для ряда с картинками. 5. Для блока title изменен размер шрифта для моб устройств.

--- assets/dist/js/advice.js
+++ assets/dist/js/advice.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-
+   $('.equipment__carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      dotsClass: 'equipment__dots',
+      customPaging : function(slider, i) {
+         var current = i + 1;
+         current = current < 10 ? "0" + current : current;
+         var thumb = $(slider.$slides[current]).data();
+         return '<button type="button" class="equipment__dot-item">'+current+'</button>';
+      },
+      fade: true
+   });
 });
