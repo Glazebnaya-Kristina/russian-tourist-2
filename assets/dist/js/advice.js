3c605653de3da07b4558eb1d1c9bc0f4b635fcb1
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-27 16:12:37 +0200

    1. Разметка секции и стили секции training. 2. Добавлен блок pagination. 3. Написан скрипт для активного класса.

--- assets/dist/js/advice.js
+++ assets/dist/js/advice.js
@@ -13,4 +13,22 @@ $(document).ready(function () {
       },
       fade: true
    });
+
+   /*--- Пагинация ---*/
+   var paginatinItem = document.querySelectorAll('.pagination li');
+
+   for( var i = 0; i < paginatinItem.length; i++){
+      paginatinItem[i].onclick = function(e){
+         e.preventDefault();
+         if(this.classList) {
+            for (var j = 0; j < paginatinItem.length; j++) {
+               paginatinItem[j].classList.remove('active');
+            }
+            this.classList.add('active');
+         } else {
+            this.active += ' ' + active;
+         }
+      }
+   }
+
 });
