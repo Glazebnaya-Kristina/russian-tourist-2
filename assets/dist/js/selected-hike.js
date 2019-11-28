49ea09a4feec124a163ef172fcc5a66bf0fa197e
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-11-28 11:35:27 +0200

    1. Разметка и стили для секции selected-hike-header. 2. Модификатор --icon для блока button. 3. Новый блок hike-info ,написаны стили. 4. Новый блок location ,написаны стили.

---
+++ assets/dist/js/selected-hike.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+   jcf.replaceAll();
+
+   jcf.setOptions('Scrollable', {
+      alwaysPreventMouseWheel: true,
+      mouseWheelStep: 15
+   });
+
+   var assistants = $('.carousel-3D__list');
+
+   assistants.on("init", function (event, slick) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+   assistants.on("afterChange", function (event, slick, currentSlide) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+   assistants.slick({
+      infinite: true,
+      slidesToShow: 3,
+      arrows: true,
+      centerMode: true,
+      centerPadding: '0',
+      prevArrow: '<button type="button" class="button-arrow carousel-3D__arrow carousel-3D__arrow--left"><i class="icon-right-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow carousel-3D__arrow carousel-3D__arrow--right"><i class="icon-right-arrow"></i></button>',
+      dots: false
+   });
+
+   var btnLike = document.querySelectorAll('.button-like'),
+      spanValue = document.querySelector('.comment__feedback span').textContent,
+      spanValueTag = document.querySelector('.comment__feedback span'),
+      btndisLike = document.querySelectorAll('.button-dislike');
+
+
+   btnLike.forEach(function (item) {
+      item.addEventListener('click', function () {
+         item.classList.add('button-like--active');
+         spanValueSum = +spanValue + 1;
+         spanValueTag.innerHTML = spanValueSum;
+      })
+   });
+   btndisLike.forEach(function (item) {
+      item.addEventListener('click', function () {
+         // btnLike.classList.remove('button-like--active');
+         item.classList.add('button-dislike--active');
+         spanValueSum = +spanValue - 1;
+         spanValueTag.innerHTML = spanValueSum;
+      })
+   });
+});
+
+
+$(document).on('click', '.button-play', function () {
+
+   var wrapperYoutube = document.querySelector('.youtube-video__overlow');
+   wrapperYoutube.classList.add('youtube-video__overlow--close');
+   var $video = $('#video'),
+      src = $video.attr('src');
+
+   $video.attr('src', src + '&autoplay=1');
+});
