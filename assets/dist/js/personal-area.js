ccadc87e59fdf8435402ef76264761de35a86eeb
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-01 14:37:04 +0200

    1. Разметка и стили для секции trekking-done 2. Разметка и стили для секции sports-tourism 3. Для блока types  добавылен элемент __img

--- assets/dist/js/personal-area.js
+++ assets/dist/js/personal-area.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
+   jcf.replaceAll();
 
+   var btnHike = document.querySelectorAll('.hike__wrapper-want');
+
+   btnHike.forEach(function (item) {
+      item.addEventListener('mouseenter', function () {
+         jcf.replaceAll();
+      });
+      item.addEventListener('click', function () {
+         item.classList.toggle('hike__wrapper-want--active');
+      })
+   });
+
+
+   jcf.setOptions('Scrollable', {
+      alwaysPreventMouseWheel: true,
+      mouseWheelStep: 15
+   });
 });
