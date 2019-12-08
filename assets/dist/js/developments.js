a9cba682084708b22fb5d953aece9f4e0ff94a77
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-08 13:59:52 +0200

    1. Везде добавлен к кнопке Личный кабинет в соб меню класс для открытия попапа. 2. Написаны стили для секции calendar. 3. Добавлены все форматы шрифтов. 4. Настроены календари. 5. Изменен полностью скрипт API гугл карты.

--- assets/dist/js/developments.js
+++ assets/dist/js/developments.js
@@ -1,3 +1,55 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//    // var calendarEl = document.getElementById('calendar');
+//    //
+//    // var calendar = new FullCalendar.Calendar(calendarEl, {
+//    //    plugins: ['dayGrid'],
+//    //    locale: 'ru',
+//    //    timeZone: 'UTC',
+//    //    header: {
+//    //       // left: 'prev,next today',
+//    //       left: '',
+//    //       // center: 'month,agendaWeek',
+//    //       center: 'title',
+//    //       right: ''
+//    //    },
+//    //    defaultDate: '2016-12-05',
+//    //    defaultView: 'month',//тебе нужно указать отображение по умолчанию
+//    //    views: {
+//    //       month: {
+//    //          titleFormat: '[любой текст] D MMMM , YYYY',//тут уже можешь добавить запятую
+//    //          titleRangeSeparator: ' to ',
+//    //       }
+//    //    },
+//    //    editable: true,
+//    //    eventLimit: true, // when too many events in a day, show the popover
+//    //    events: [
+//    //       {
+//    //          title: 'All Day Event',
+//    //          start: '2019-12-09',
+//    //          end: '2019-12-12',
+//    //          className: 'events',
+//    //       },
+//    //       {
+//    //          title: 'Встреча перед  походом ',
+//    //          start: '2019-12-22',
+//    //          end: '2019-12-22',
+//    //          className: 'meeting',
+//    //       }
+//    //    ]
+//    // });
+//
+//
+//    // calendar.render();
+//
+//
+//    // document.getElementById('my-today-button').addEventListener('click', function () {
+//    //    calendar.today();
+//    // });
+//
+//
+//
+// });
+
 $(document).ready(function () {
    /*--- Пагинация ---*/
    var paginatinItem = document.querySelectorAll('.pagination li');
@@ -15,4 +67,129 @@ $(document).ready(function () {
          }
       }
    }
+
+   // календарь
+   $('#calendar').fullCalendar({
+      // plugins: ['dayGrid'],
+      firstDay: 1,
+      header: {
+         // left: '',
+         left: 'today',
+         center: '',
+         right: 'title'
+      },
+      monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      monthNamesShort: ['Янв.', 'Фев.', 'Март', 'Апр.', 'Май', 'Июнь', 'Июль', 'Авг.', 'Сент.', 'Окт.', 'Ноя.', 'Дек.'],
+      dayNames: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+      dayNamesShort: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+      defaultDate: '2019-12-07',
+      defaultView: 'month',//тебе нужно указать отображение по умолчанию
+      fixedWeekCount: false,
+      views: {
+         month: {
+            titleFormat: 'MMMM, YYYY',//тут уже можешь добавить запятую
+            titleRangeSeparator: ' to ',
+         }
+      },
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+         {
+            title: 'Чемпионат России по водному туризму',
+            start: '2019-12-09',
+            end: '2019-12-13',
+            className: 'events-block'
+         },
+         {
+            title: 'Чемпионат России по водному туризму',
+            start: '2019-12-19',
+            end: '2019-12-24',
+            className: 'events-block'
+         },
+         {
+
+            title: 'Встреча перед  походом ',
+            start: '2019-12-25',
+            end: '2019-12-25',
+            className: 'meeting',
+            url: 'https://www.google.com/',
+            customRender: true
+         }
+      ],
+
+      eventRender: function(event, element, view) {
+         if (event.customRender === true)
+         {
+            var dataToFind = moment(event.start).format('YYYY-MM-DD');
+            $("td[data-date='"+dataToFind+"']").addClass('activeDay');
+            console.log(dataToFind)
+         }
+      },
+   });
+
+
+   // цвет title
+   $('.fc-right h2').each(function (index, element) {
+      var heading = $(element);
+      var Word_array, last_Word, first_part;
+
+      Word_array = heading.html().split(/\s+/); // split on spaces
+      last_Word = Word_array.pop();             // pop the last Word
+      first_part = Word_array.join(' ');        // rejoin the first words together
+
+      heading.html([first_part, ' <span class="last-Word">', last_Word, '</span>'].join(''));
+   });
+
+   $('.fc-right').append("<span class='open-calendar'></span>");
+
+   $('.fc-right h2 .last-Word').css('color', '#9a9a9a');
+
+
+   // текущее время
+   var d = new Date();
+
+   var mnt = new Array("января", "февраля", "марта", "апреля", "мая",
+      "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря");
+
+   var todeyValue = $('.fc-today .fc-day-number').text();
+
+   $('.fc-today-button ').html('<p class="today-btn">Сегодня</p><span>' + todeyValue + ' ' + mnt[d.getMonth()] + '</span>');
+
+
+   $('.calendar__arrow--left').on('click', function () {
+      $('#calendar').fullCalendar('prev'); // call method
+   });
+
+   $('.calendar__arrow--right').on('click', function () {
+      $('#calendar').fullCalendar('next'); // call method
+   });
+
+
+
+   //каледарь маленький
+   $(".datepicker-here").datepicker({
+      firstDay: 1,
+      changeMonth: true,
+      changeYear: true,
+      altFormat: "MMMM, dd",
+      monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      onChangeMonthYear: function (year, month, inst) {
+         var date = new Date();
+         $('#calendar').fullCalendar('gotoDate', date);
+      },
+      onSelect: function (dateText, inst) {
+         var date = new Date(dateText);
+         $('#calendar').fullCalendar('gotoDate', date);
+      }
+   });
+
+   var openCalendar = document.querySelector('.open-calendar');
+   var calendarBlock = document.querySelector('.datepicker-here');
+
+   openCalendar.addEventListener('click', function () {
+      calendarBlock.classList.toggle('datepicker-here--opened');
+   })
+
 });
--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -5,181 +5,166 @@ $(function () {
       dots: true
    });
 
+   //карта
+   var map;
+   var infoWindow;
 
    function initMap() {
-
-      var bluhera = {lat: 57.6686531, lng: 46.5856643};
-      var ducy = {lat: 49.5509507, lng: 25.5893844};
-      var ipp = {lat: 47.9547219, lng: 21.7162095};
-      var mayak = {lat: 44.8017258, lng: 10.318929};
-      var mokko = {lat: 36.5563376, lng: 2.7858127};
-      var tbilici = {lat: 41.7136682, lng: 44.7907789};
-      var yzbek = {lat: 46.517961, lng: 58.387727};
-
-      var map = new google.maps.Map(
-         document.getElementById('map'), {zoom: 11, center: bluhera});
-
-      var marker1 = new google.maps.Marker({position: bluhera, map: map, icon: '../img/flag.png'});
-      var marker2 = new google.maps.Marker({position: ducy, map: map, icon: '../img/flag.png'});
-      var marker3 = new google.maps.Marker({position: ipp, map: map, icon: '../img/flag.png'});
-      var marker4 = new google.maps.Marker({position: mayak, map: map, icon: '../img/flag.png'});
-      var marker5 = new google.maps.Marker({position: mokko, map: map, icon: '../img/flag.png'});
-      var marker6 = new google.maps.Marker({position: tbilici, map: map, icon: '../img/flag.png'});
-      var marker7 = new google.maps.Marker({position: yzbek, map: map, icon: '../img/flag.png'});
-
-      var StreetBlu = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-      var StreetDucy = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-      var StreetIpp = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-      var StreetMayak = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-      var StreetMokko = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-      var StreetTbilici = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-      var Streetyzbek = '<div id="content" class="map-popup">' +
-         '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
-         '<h3 class="map-popup__title">Название маршрута</h3>' +
-         '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
-         '<ul class="map-popup__list">'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
-         '<li class="map-popup__item">Абхазия</li>'+
-         '<li class="map-popup__item">Казбек</li>'+
-         '<li class="map-popup__item">Тридцатка</li>'+
-         '<li class="map-popup__item">Сванетия</li>'+
-         '</ul>'+
-         '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
-         '</div>';
-
-      // Создаем информационное окно
-      var infoBlu = new google.maps.InfoWindow({
-         content: StreetBlu,
-         maxWidth: 355
-      });
-      var infoDucy = new google.maps.InfoWindow({
-         content: StreetDucy,
-         maxWidth: 400
-      });
-      var infoIpp = new google.maps.InfoWindow({
-         content: StreetIpp,
-         maxWidth: 400
-      });
-      var infoMayak = new google.maps.InfoWindow({
-         content: StreetMayak,
-         maxWidth: 400
-      });
-      var infoMokko = new google.maps.InfoWindow({
-         content: StreetMokko,
-         maxWidth: 400
-      });
-      var infoTbilici = new google.maps.InfoWindow({
-         content: StreetTbilici,
-         maxWidth: 400
-      });
-      var infoyzbek = new google.maps.InfoWindow({
-         content: Streetyzbek,
-         maxWidth: 400
+      map = new google.maps.Map(document.getElementById('map'), {
+         center: {lat: 50.4403076, lng: 30.5187587},
+         zoom: 5,
+         icon: '../img/flag.png'
       });
 
-      // Создаем прослушивание, по клику на маркер - открыть инфо-окно infowindow
-      marker1.addListener('click', function () {
-         infoBlu.open(map, marker1);
-         // marker1 = new google.maps.Marker({position: bluhera, map: map, icon: '../img/user.png'});
+      infoWindow = new google.maps.InfoWindow({
+         pixelOffset: new google.maps.Size(0, 70)
+      });
+      addMarkers();
+
+      map.addListener('click', function() {
+         if (infoWindow) infoWindow.close();
+      });
+   }
+
+
+   function addMarkers() {
+      addMarker({
+         coords: { lat: 57.6686531, lng: 46.5856643 },
+         content: '<div id="content" class="map-popup">' +
+               '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+               '<h3 class="map-popup__title">Название маршрута</h3>' +
+               '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+               '<ul class="map-popup__list">'+
+               '<li class="map-popup__item">Сванетия</li>'+
+               '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+               '<li class="map-popup__item">Абхазия</li>'+
+               '<li class="map-popup__item">Казбек</li>'+
+               '<li class="map-popup__item">Тридцатка</li>'+
+               '<li class="map-popup__item">Сванетия</li>'+
+               '</ul>'+
+               '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+               '</div>',
+      });
+      addMarker({
+         coords: { lat: 49.5509507, lng: 25.5893844 },
+         content: '<div id="content" class="map-popup">' +
+            '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+            '<h3 class="map-popup__title">Название маршрута</h3>' +
+            '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+            '<ul class="map-popup__list">'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+            '<li class="map-popup__item">Абхазия</li>'+
+            '<li class="map-popup__item">Казбек</li>'+
+            '<li class="map-popup__item">Тридцатка</li>'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '</ul>'+
+            '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+            '</div>',
+      });
+      addMarker({
+         coords: { lat: 47.9547219, lng: 21.7162095 },
+         content: '<div id="content" class="map-popup">' +
+            '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+            '<h3 class="map-popup__title">Название маршрута</h3>' +
+            '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+            '<ul class="map-popup__list">'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+            '<li class="map-popup__item">Абхазия</li>'+
+            '<li class="map-popup__item">Казбек</li>'+
+            '<li class="map-popup__item">Тридцатка</li>'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '</ul>'+
+            '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+            '</div>',
+      });
+      addMarker({
+         coords: { lat: 44.8017258, lng: 10.318929 },
+         content: '<div id="content" class="map-popup">' +
+            '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+            '<h3 class="map-popup__title">Название маршрута</h3>' +
+            '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+            '<ul class="map-popup__list">'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+            '<li class="map-popup__item">Абхазия</li>'+
+            '<li class="map-popup__item">Казбек</li>'+
+            '<li class="map-popup__item">Тридцатка</li>'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '</ul>'+
+            '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+            '</div>',
+      });
+      addMarker({
+         coords: { lat: 36.5563376, lng: 2.7858127 },
+         content: '<div id="content" class="map-popup">' +
+            '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+            '<h3 class="map-popup__title">Название маршрута</h3>' +
+            '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+            '<ul class="map-popup__list">'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+            '<li class="map-popup__item">Абхазия</li>'+
+            '<li class="map-popup__item">Казбек</li>'+
+            '<li class="map-popup__item">Тридцатка</li>'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '</ul>'+
+            '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+            '</div>',
+      });
+      addMarker({
+         coords: { lat: 41.7136682, lng: 44.7907789 },
+         content: '<div id="content" class="map-popup">' +
+            '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+            '<h3 class="map-popup__title">Название маршрута</h3>' +
+            '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+            '<ul class="map-popup__list">'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+            '<li class="map-popup__item">Абхазия</li>'+
+            '<li class="map-popup__item">Казбек</li>'+
+            '<li class="map-popup__item">Тридцатка</li>'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '</ul>'+
+            '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+            '</div>',
+      });
+      addMarker({
+         coords: { lat: 46.517961, lng: 58.387727 },
+         content: '<div id="content" class="map-popup">' +
+            '<p class="map-popup__img"><img src="../img/map-img.jpg" alt="Фотография местости"></p>'+
+            '<h3 class="map-popup__title">Название маршрута</h3>' +
+            '<p class="map-popup__level"><span>Сложность:</span><img src="../img/trekking/level.png" alt="уровень сложности"></p>'+
+            '<ul class="map-popup__list">'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '<li class="map-popup__item">Эльбрус восточная вершина</li>'+
+            '<li class="map-popup__item">Абхазия</li>'+
+            '<li class="map-popup__item">Казбек</li>'+
+            '<li class="map-popup__item">Тридцатка</li>'+
+            '<li class="map-popup__item">Сванетия</li>'+
+            '</ul>'+
+            '<a href="article-post.html" class="map-popup__link"><i class="icon-right-arrow right-arrow right-arrow--blue"></i></a>'+
+            '</div>',
+      });
+   }
+
+
+   var img1 = "../img/flag.png";
+
+   function addMarker(props) {
+      var marker = new google.maps.Marker({
+         position: props.coords,
+         map: map,
+         icon: img1
       });
 
-      marker2.addListener('click', function () {
-         infoDucy.open(map, marker2);
-      });
-      marker3.addListener('click', function () {
-         infoIpp.open(map, marker3);
-      });
-      marker4.addListener('click', function () {
-         infoMayak.open(map, marker4);
-      });
-      marker5.addListener('click', function () {
-         infoMokko.open(map, marker5);
-      });
-      marker6.addListener('click', function () {
-         infoTbilici.open(map, marker6);
-      });
-      marker7.addListener('click', function () {
-         infoyzbek.open(map, marker7);
-      });
+      if (props.content) {
+         marker.addListener('click', function () {
+            infoWindow.setContent(props.content);
+            infoWindow.open(map, marker);
+         });
+      }
    }
 
    initMap();
